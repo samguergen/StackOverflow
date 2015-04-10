3082aa4c4883537681c8ca8440c6e64aceac465a
Hoa Nguyen & Kevin Alwell & Malcolm Bouzi & Awesome Ninja Fighter <pair+hoathenguyen85.kevalwell.mbouzi.samguergen@devbootcamp.com>
2015-04-10 16:39:07 -0400

    set database relationships

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+  has_many :answers
+  belongs_to :creator, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :questions, foreign_key: 'creator_id'
+  has_many :answers
 end
