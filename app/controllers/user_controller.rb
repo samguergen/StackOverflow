8847f11c82c37049ddbe9c9e5f6d362876a9811d
Kevin Alwell <kevalwell@gmail.com>
2015-04-10 17:25:58 -0400

    Created User Essentials

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -48,16 +48,21 @@ class UserController < ApplicationController
 
 
 #KEVIN STARTS WORKING FROM HERE DOWN
-def create
-  @user = user.create(params)
-end
 
 def new
-
+  @user = User.new
 end
 
+def create
+  @user = User.new(params[:user])
 
-
+  if @user.save
+    flash[:notice] = "Account registered!"
+    redirect_back_or_default account_url
+  else
+    render :action => :new
+  end
+end
 
 
 
