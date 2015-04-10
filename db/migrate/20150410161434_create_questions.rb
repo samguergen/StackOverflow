e50a028570dc0912bd4cefa49df9a554b19b15d4
Hoa Nguyen & Kevin Alwell & Malcolm Bouzi & Awesome Ninja Fighter <pair+hoathenguyen85.kevalwell.mbouzi.samguergen@devbootcamp.com>
2015-04-10 12:24:07 -0400

    complete question/answer migrations

--- db/migrate/20150410161434_create_questions.rb
+++ db/migrate/20150410161434_create_questions.rb
@@ -1,6 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.references :creator, class_name: 'User'
+      t.string :title
+      t.string :content
+      t.references :best_answer, class_name: 'Answer'
 
       t.timestamps null: false
     end
--- db/migrate/20150410161442_create_answers.rb
+++ db/migrate/20150410161442_create_answers.rb
@@ -1,6 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.string :content
+      t.references :question
+      t.references :user
 
       t.timestamps null: false
     end
