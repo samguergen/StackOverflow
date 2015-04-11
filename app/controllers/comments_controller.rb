16f10c399caa9595a79a4ccdf91a6e17d5905700
Alwell <kevalwell@gmail.com>
2015-04-11 18:32:10 -0400

    altered comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,19 @@
 class CommentsController < ApplicationController
   #INDENTED FOR HOA'S LOVE
 
+    def index
+      @comments = Comments.all
+    
+    end
+
   def new
     @comment = Comment.new
   end
 
   def create
-    @question = Question.find_by(question_id: params[:question_id])
     @new_comment = Comment.new(comment_params)
     if @new_comment.save!
+      flash[:notice] = "Thank You for Commenting!"
       redirect_to question_path(@question)
     else
       flash[:notice] = "A problem has occurred. Your comment couldn't be posted."
