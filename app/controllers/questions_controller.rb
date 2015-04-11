ca5bdd20aca10bd00f98d26435ffd9272cc753f3
Samantha Guergenenov <samguergen@gmail.com>
2015-04-11 13:42:59 -0400

    finished destroy method for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,13 @@
 class QuestionsController < ApplicationController
+
   def new
     @question = Question.new
+  end
+
+  def create
+    @new_question = Question.new(question_params)
+    if @new_question.save!
+
   end
 
   def show
@@ -10,4 +17,16 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if session[:user_id] == @question.user_id
+      @question.destroy!
+      redirect_to questions_path
+    else
+      flash[:notice] = "You can only delete your own questions!"
+      redirect_to questions_path
+    end
+  end
+
 end
