28b0f329ed08305dfab3ed281a1a0095da81944a
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-10 19:42:22 -0500

    Fix routes by merging into resource block 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users
+  resources :users do
+    collection do
+      get 'login'
+      post 'new_login'
+    end
+  end
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
-   post '/users/new' => 'user#create'
-
-  get '/login' => 'users#login', as: :login
-
-  post '/login' => 'users#new_login', as: :new_login
-
-
-
-
-
-
+  root 'welcome#index'
 
 
   resources :questions
