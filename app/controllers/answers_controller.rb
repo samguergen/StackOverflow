5280d2b2a31a39c2dcad4f6b46b449fe1733879c
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-10 19:46:50 -0500

    Save answer from question#show 	modified:   app/controllers/answers_controller.rb

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,8 @@
 class AnswersController < ApplicationController
   def create
     answer = Answer.new(answer_params)
+    answer.user_id = session[:user_id]
+    answer.save
 
     redirect_to answer.question
   end
