5087c2d61b600affe1d78158e90578bd5c5eb96c
Samantha Guergenenov <samguergen@gmail.com>
2015-04-11 21:28:21 -0400

    delete unnecessary method for downpost- all is working for votes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   resources :answers do
     resources :votes
-      post '/downvote' => 'votes#downvote', as: :answer_down
+
   end
   resources :questions do
     resources :votes
-        post '/downvote' => 'votes#downvote', as: :question_down
+
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
