831556a80c49ba46c2f60f4e8f07493622be4850
Samantha Guergenenov <samguergen@gmail.com>
2015-04-11 21:01:52 -0400

    vote links working but like and dislike links cannot both display on same page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,29 @@ class VotesController < ApplicationController
 
 
 
+  def new
+    @vote = Vote.new
+  end
 
+  def create
+    p "Over Here!!!"
+    p "#{params[:test]}"
+    @question = params[:question_id]
 
+    if params[:like] == "yes"
+    @vote = Vote.create!(user_id: session[:user_id], votable_id: params[:question_id], votable_type: "Question" , point: 1)
+
+    elsif params[:like] == "no"
+    @vote = Vote.create!(user_id: session[:user_id], votable_id: params[:question_id], votable_type: "Question", point: -1)
+    # @question = @vote.question
+  #   if @vote.save!
+  #     redirect_to question_path(@question)
+  #   else
+  #     flash[:notice] = "You can't vote!"
+  #     redirect_to question_path(@question)
+  #   end
+    end
+    redirect_to question_path(@question)
+  end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
+
+  def self.total(num, kind)
+    Vote.where(votable_id: num, votable_type: kind).sum(:point)
+  end
+
 end
